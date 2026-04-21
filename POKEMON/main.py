331666deed8a52244acf7a2b69b972100f193af9
cotocesteban-web <cotoc.esteban@gmail.com>
2026-04-21 13:07:54 -0600

    .

--- POKEMON/main.py
+++ POKEMON/main.py
@@ -11,7 +11,7 @@ def crear_pokemon(opcion):
         ### aca accedo a los datos del pokemon que esta en el catalogo
         datos = pokedex.CATALOGO_POKEMON[opcion]
         tipo = datos["tipo"]
-        nombre =datos["nombre"]
+        nombre = datos["nombre"]
         hp = datos["hp_maximo"]
         energia_max = datos["energia maxima"]
 
@@ -39,7 +39,35 @@ def jugar():
         # hacemos las comparaciones segun el modod de juego elegido por el usuario:
         if modo == "2":
             # si ingresso la opcion 2. puede seleccionar su personaje.
+            eleccion_jugador = input ("elige el numeronde tu Pokemon: ")
+            jugador = crear_pokemon(eleccion_jugador)
 
+            selecion_compu = random.randrange(1,8)
+            computadora = crear_pokemon(selecion_compu)
 
+            if not jugador or not computadora:
+                print("Erron en la seleccion.")
+                return
+            print(f"\n{jugador.nombre} VS {computadora.nombre} (computadora)")
 
+        ### iniciar un bucle para hacer valido que el hp no sea 0
+        while jugador.hp_actual > 0 and computadora.hp_actual > 0:
+            # Mostrar el turno de jugador
+            print(f"\n Turno de {jugador.nombre}")
+            accion = input("Que accion deseas hacer?")
+
+            if accion == "1":
+                jugador.atacar(computadora)
+
+            else:
+                print("opcion invalida, espera nuevamente el turno")
+
+        
+           
+        
     
+
+
+
+
+
