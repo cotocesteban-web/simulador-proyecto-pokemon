123c2163bac73f34c17b407eea0cb3ccfaeadda1
cotocesteban-web <cotoc.eteban@gmail.com>
2026-04-20 23:19:45 -0600

    .....

--- POKEMON/main.py
+++ POKEMON/main.py
@@ -1,2 +1,31 @@
+import random
 import pokedex
-import pokemon_clases
+from pokemon_clases import PokemonFuego, PokemonAgua, PokemonPlanta, PokemonElectrico
+
+def crear_pokemon(opcion):
+    
+    ##Creamos una instancia de un Pokemon basada en la opción seleccionada.
+    
+    # Convertimos a string por si acaso, ya que las llaves en pokedex.py son "1", "2", etc.
+    opcion_str = str(opcion)
+    
+    if opcion_str in pokedex.CATALOGO_POKEMON:
+        datos = pokedex.CATALOGO_POKEMON[opcion_str]
+        tipo = datos["tipo"]
+        nombre = datos["nombre"]
+        hp = datos["hp_maximo"]
+        en = datos["energia_maxima"]
+
+        # Instanciamos la clase correcta según el tipo
+        if tipo == "Fuego":
+            return PokemonFuego(nombre, hp, en)
+        elif tipo == "Agua":
+            return PokemonAgua(nombre, hp, en)
+        elif tipo == "Planta":
+            return PokemonPlanta(nombre, hp, en)
+        elif tipo == "Electrico":
+            return PokemonElectrico(nombre, hp, en)
+    else:
+        print(f"La opción {opcion_str} no es válida.")
+        return 
+
