54e46d5add51e64dc6289766fdf8d025621a5598
cotocesteban-web <cotoc.eteban@gmail.com>
2026-04-22 03:20:45 -0600

    .

--- POKEMON/main.py
+++ POKEMON/main.py
@@ -1,11 +1,15 @@
+### ** Este es el archivo Main (Principal) hara que ejecute todo lo que hemo llevado con los dos archivos
+## Importamos ramdom para que nos elija un numero aleatorio del rando que le indiquemos.
 import random
+# Importar el pokedex que es donde esta el diccionario de todo los pokemones con sus caracteristicas:
 import pokedex
+# De pokemon clases importamos especificamente los pokemones.
 from pokemon_clases import PokemonFuego, PokemonAgua, PokemonPlanta, PokemonElectrico
+
 ### aca creamos el pokemon: 
 def crear_pokemon(opcion):
-    opcion = input("Selecciona: ")
-    if opcion.isdigit():
-        opcion = int(opcion)
+
+    opcion = str(opcion)
     
     if opcion in pokedex.CATALOGO_POKEMON:
         ### aca accedo a los datos del pokemon que esta en el catalogo
@@ -13,66 +17,112 @@ def crear_pokemon(opcion):
         tipo = datos["tipo"]
         nombre = datos["nombre"]
         hp = datos["hp_maximo"]
-        energia_max = datos["energia maxima"]
+        energia_max = datos["energia_maxima"]
 
         #
-    if tipo == "Fuego":
-        return PokemonFuego(nombre, hp, energia_max)
-    elif tipo == "Agua":    
-        return PokemonAgua(nombre, hp, energia_max)
-    elif tipo == "Planta":
-        return PokemonAgua(nombre, hp, energia_max)
-    elif tipo == "Electrico":
-        return PokemonElectrico(nombre, hp, energia_max)
+        if tipo == "Fuego":
+            return PokemonFuego(nombre, hp, energia_max)
+        elif tipo == "Agua":    
+            return PokemonAgua(nombre, hp, energia_max)
+        elif tipo == "Planta":
+            return PokemonPlanta(nombre, hp, energia_max)
+        elif tipo == "Electrico":
+            return PokemonElectrico(nombre, hp, energia_max)
     else:
         print(f"La opcion {opcion} no es valida.")
         return
     
 def jugar():
-        ## mostrar los modos de juego al usuario:
-        print("1. Jugador vs Jugador")
-        print("2. Jugador vs Computadora")
-        modo = input("Porfavor Seleccione el modo de juego: ")
-        # mostrar el catalogo diponibles de pokemones imprimiendo en pantala:
-        pokedex.mostrar_catalogo_disponible()
-     
-        # hacemos las comparaciones segun el modod de juego elegido por el usuario:
-        if modo == "2":
-            # si ingresso la opcion 2. puede seleccionar su personaje.
-            eleccion_jugador = input ("elige el numeronde tu Pokemon: ")
-            jugador = crear_pokemon(eleccion_jugador)
+    ## mostrar los modos de juego al usuario:
+    print("1. Jugador vs Jugador")
+    print("2. Jugador vs Computadora")
+    modo = input("Porfavor Seleccione el modo de juego: ")
+    # mostrar el catalogo diponibles de pokemones imprimiendo en pantala:
+    pokedex.mostrar_catalogo_disponible()
+    # eleccion de los personajes
+     ## Ete e el modo jugador vs Jugador
+    if modo == "1":
+        eleccion1 = input("Jugador 1, elije el numero de tu Pokemon: ")
+        jugador = crear_pokemon(eleccion1)
+        eleccion2 = input("Jugador 2, elige el numero de tu Pokemon; ")
+        jugador2 = crear_pokemon(eleccion2)
+    # Jugador vs computadora:
+    elif modo == "2":
+        # Jugador vs computadora:
+        eleccion1 = input("Elije el numero de tu Pokemon: ")
+        jugador = crear_pokemon(eleccion1)
+        # Seleccion Aleatoria por random:
+        eleccion_pc = str(random.randint(1, 8))
+        jugador2 = crear_pokemon(eleccion_pc)
 
-            selecion_compu = random.randrange(1,8)
-            computadora = crear_pokemon(selecion_compu)
+    else: 
+        print("Modo invalido")
 
-            if not jugador or not computadora:
-                print("Erron en la seleccion.")
-                return
-            print(f"\n{jugador.nombre} VS {computadora.nombre} (computadora)")
+        ## Mostrar oponente en los mensajes
+    if modo =="1":
+        rival = "(Jugador vs jugador)"
+    else:
+        rival = "(Jugador vs Pc)"
 
-        ### iniciar un bucle para hacer valido que el hp no sea 0
-        while jugador.hp_actual > 0 and computadora.hp_actual > 0:
-            # Mostrar el turno de jugador
-            print(f"\n Turno de {jugador.nombre}")
-            # Aca guardamos lo qu el ususrio ingresa sobre que accion tomar
-            accion = input("Que accion deseas hacer?")
+    print(f"\n {jugador.nombre} vs {jugador2.nombre} {rival}")
 
-            if accion == "1":
-                jugador.atacar(computadora)
-            elif accion == "2":
-                jugador.defender()
-            elif accion == "3":
-                jugador.descansar()
+    ## Comienzan las batallas mientra que ambos tengan vida
+    while jugador.hp_actual > 0 and jugador2.hp_actual > 0:
+
+        ## Turno del jugador 1;:
+        print(f"\n Turno de {jugador.nombre} ")
+        print(f" HP {jugador.hp_actual} | Energia: {jugador.energia_actual}")
+        accion = input("Selecciona (1. Atacar, 2.Defender, 3.Descansar:)")
+
+        if accion == "1":
+            jugador.atacar(jugador2)
+        elif accion == "2":
+            jugador.defender()
+        elif accion == "3":
+            jugador.descansar()
+
+        else:
+            print("Opcion invalida, pierdes el turno: ")
+
+
+            ## Verificar el estado del Jugador 2\
+        if jugador2.hp_actual <= 0:
+            print(f"\2 {jugador.nombre} Ha ganado la batalla")
+            break
+
+             # turno del jUGADOR 2
+        print(F"\n  Turno de {jugador2.nombre} {rival} ")
+        print(f"Hp {jugador2.hp_actual} | Energia:{jugador2.energia_actual}")
+        
+        if modo == "1":
+            ## el Jugador2
+            accion2 = input("Selecciona(1. Atacar, 2.Defender, 3.Decansar):")
+            if accion2 == "1":
+                jugador2.atacar(jugador)
+            elif accion2 == "2":
+                jugador2.defender()
+            elif accion2 =="3":
+                jugador2.descansar()
+            else:
+                print("Opcion invalida, pierdes turno")
+            # Si es computadora;
+        else: 
+            accion_pc_eleccion = random.choice(["atacar", "atacar", "descansar"])
+            if accion_pc_eleccion == "atacar":
+                jugador2.atacar(jugador)
 
             else:
-                print("opcion invalida, espera nuevamente el turno")
+                jugador2.descansar()
 
-                ## Moatrar si la pc aun le quedan Energy points:
-            if computadora.hp_actual <= 0:
-                print(f"\n{computadora.nombre} ya no tiene Energy Points")
+            ## verificar i el jugador 1 murio:
+        if jugador2.hp_actual <= 0:
+          print(f"\n {jugador2.nombre} {rival} ha ganado la batalla")
+  
 
-                #
-                accion_computadora = random.choice([]
+if __name__ == "__main__":
+    jugar()   
+
+
 
     
 
--- POKEMON/pokemon_clases.py
+++ POKEMON/pokemon_clases.py
@@ -1,6 +1,8 @@
+## importamo ramdom
 import random
+## el abtractmethhod el contrato que llevaran la hijas
 from abc import ABC, abstractmethod
-
+## clase padre
 class Pokemon(ABC):
     def __init__(self, nombre, hp_maximo, energia_maxima):
         self.nombre = nombre
@@ -9,7 +11,7 @@ class Pokemon(ABC):
         self.__hp_actual = hp_maximo
         self.__energia_actual = energia_maxima
         self.defendiendo = False
-
+## Las propiedades privadas se hacen con Property
     @property
     def hp_actual(self):
         return self.__hp_actual
@@ -18,6 +20,7 @@ class Pokemon(ABC):
     def hp_actual(self, valor):
         if valor < 0:
             self.__hp_actual = 0
+            print("Ha perdido")
         elif valor > self.__hp_maximo:
             self.__hp_actual = self.__hp_maximo
         else:
@@ -35,7 +38,7 @@ class Pokemon(ABC):
             self.__energia_actual = self.__energia_maxima
         else:
             self.__energia_actual = valor
-
+## funcion defender
     def defender(self):
         if self.energia_actual >= 5:
             self.energia_actual -= 5
@@ -43,11 +46,11 @@ class Pokemon(ABC):
             print(f"{self.nombre} se esta defendiendo.")
         else:
             print("No tiene energia suficiente.")
-
+## funcion descansar
     def descansar(self):
         self.energia_actual += 20
-        print(f"{self.nombre} descansa y recupera energia.")
-
+        print(f"{self.nombre} descansa y vueleve a recuperar energia. HP acutual{self .hp_actual}")
+# funcion que calcula el da;o
     def recibir_dano(self, dano):
         if self.defendiendo:
             dano = dano // 2
@@ -55,16 +58,18 @@ class Pokemon(ABC):
 
         self.hp_actual -= dano
         print(f"{self.nombre} recibe {dano} de danio.")
-
+# el metodo abstracto que va a atacar solamente
     @abstractmethod
     def atacar(self, oponente):
         pass
 
-
+# clase hija
 class PokemonAgua(Pokemon):
+    # el constructor
     def __init__(self, nombre, hp_maximo, energia_maxima):
+        # el super e para que si se ejecute
         super().__init__(nombre, hp_maximo, energia_maxima)
-
+# metodo atacar con obreecitura
     def atacar(self, oponente):
         if self.energia_actual < 15:
             print("No hay energia suficiente.")
@@ -72,14 +77,14 @@ class PokemonAgua(Pokemon):
 
         self.energia_actual -= 15
         dano = 10
-
+# El isintnace
         if isinstance(oponente, PokemonFuego):
             dano *= 2
             print("¡Es super efectivo!")
 
         oponente.recibir_dano(dano)
 
-
+# clase hija
 class PokemonFuego(Pokemon):
     def __init__(self, nombre, hp_maximo, energia_maxima):
         super().__init__(nombre, hp_maximo, energia_maxima)
@@ -98,14 +103,14 @@ class PokemonFuego(Pokemon):
 
         oponente.recibir_dano(dano)
 
-
+# clase hija
 class PokemonPlanta(Pokemon):
     def __init__(self, nombre, hp_maximo, energia_maxima):
         super().__init__(nombre, hp_maximo, energia_maxima)
 
     def atacar(self, oponente):
         if self.energia_actual < 15:
-            print("No tiene energia sufieciente")
+            print("No tiene energia suficiente")
             return
 
         self.energia_actual -= 15
@@ -116,7 +121,7 @@ class PokemonPlanta(Pokemon):
             print("¡Es efectivo!")
 
         oponente.recibir_dano(dano)
-
+# clae hija
 class PokemonElectrico(Pokemon):
     def __init__(self, nombre, hp_maximo, energia_maxima):
         super().__init__(nombre, hp_maximo, energia_maxima)
@@ -128,9 +133,11 @@ class PokemonElectrico(Pokemon):
 
         self.energia_actual -= 15
         dano = 10
+        ## ramdom escoje en aleatorio
 
         if random.random() < 0.2:
             print("Paralizado")
+            dano = 20
 
         oponente.recibir_dano(dano)
 
